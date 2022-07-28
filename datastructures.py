f0aebdf8e07b68c0924b3c88bcebceabca5e8dd3
Alexander Gress <alexander.gress@helmholtz-hips.de>
2022-07-28 20:02:41 +0200

    Added functionality for sequence cluster tree Can now be created from any fasta file using mmseqs clustering

--- datastructures.py
+++ datastructures.py
@@ -1,7 +1,7 @@
 import os
+import time
 
-import utils
-
+from utils import randomString, seqMapToFasta, call_mmseqs_clustering
 
 class Environment:
     def __init__(self, input_file, steps, out_dir, fasta_store, tr_size, te_size):
@@ -21,9 +21,13 @@ class Environment:
         if not os.path.isdir(self.tmp_folder):
             os.mkdir(self.tmp_folder)
 
+        self.mmseqs2_path = 'mmseqs'
+
+        self.verbosity = 3
 
 class Mmseqs_cluster:
-    def __init__(self, cluster_file):
+    def __init__(self, cluster_file, seq_id_threshold):
+        self.seq_id_threshold = seq_id_threshold
         f = open(cluster_file, 'r')
         lines = f.readlines()
         f.close()
@@ -31,7 +35,7 @@ class Mmseqs_cluster:
         self.clusters = {}
 
         for line in lines:
-            words = line.split('\t')
+            words = line[:-1].split('\t')
             if len(words) != 2:
                 continue
             cluster_head, cluster_member = words
@@ -39,17 +43,244 @@ class Mmseqs_cluster:
                 self.clusters[cluster_head] = []
             self.clusters[cluster_head].append(cluster_member)
 
+    def print_all(self):
+        for cluster in self.clusters:
+            print(f'Rep: {cluster}')
+            for member in self.clusters[cluster]:
+                print(f'  {member}')
+
+
+def get_mmseqs_cluster(env, input_file, seq_id_threshold = 0.0, cleanup = True):
+    cluster_file, rep_seq_file, all_seq_file = call_mmseqs_clustering(env, input_file, seq_id_threshold = seq_id_threshold)
+    cluster_obj = Mmseqs_cluster(cluster_file, seq_id_threshold)
+
+    if cleanup:
+        os.remove(cluster_file)
+        os.remove(rep_seq_file)
+        os.remove(all_seq_file)
+
+    return cluster_obj
+
+def make_fasta(sequence_map, env, subset = None):
+    rstring = randomString()
+    fasta_file = f'{env.tmp_folder}/{rstring}.fasta'
+    seqMapToFasta(sequence_map, fasta_file, subset = subset)
+    return fasta_file
 
 class Sequence_cluster_tree:
 
-    def __init__(self, sequence_map, tmp_folder, initial_fasta_file = None):
+    class Node:
+        def __init__(self, label, children = None, fused_children = None):
+            self.label = label
+            if children is None:
+                self.children = []
+            else:
+                self.children = children
+
+            if fused_children is None:
+                self.fused_children = []
+            else:
+                self.fused_children = fused_children
+
+        def isLeaf(self):
+            return len(self.children) == 0
+
+        def fuse_check(self, sequence_map):
+            if self.isLeaf():
+                return
+            seq_a = sequence_map[self.children[0]]
+            for child in self.children[1:]:
+                seq_b = sequence_map[child]
+                if seq_a != seq_b:
+                    return
+            self.fused_children = self.children[0:]
+            self.children = []
+
+        def get_fused_label(self):
+            return ' & '.join([f'{self.label}'] + [str(x) for x in self.fused_children])
+
+        def print_cascade(self, nodes, level = 0):
+            print(f'{" "*level}{self.get_fused_label()}')
+            for child in self.children:
+                nodes[child].print_cascade(nodes, level = level+1)
+
+
+    def __init__(self, sequence_map, env, initial_fasta_file = None):
+
+        if env.verbosity >= 1:
+            print(f'Creating new sequence cluster tree: {initial_fasta_file}')
+            t0 = time.time()
+
         if initial_fasta_file is not None:
             fasta_file = initial_fasta_file
         else:
-            rstring = utils.randomString()
-            fasta_file = f'{tmp_folder}/{rstring}.fasta'
+            fasta_file = make_fasta(sequence_map, env)
 
-        
+        cluster_obj = get_mmseqs_cluster(env, fasta_file, seq_id_threshold = 0.5)
 
-        if initial_fasta_file is not None:
+        if initial_fasta_file is None:
+            if env.verbosity >= 1:
+                print(f'Removing file: {fasta_file}')
             os.remove(fasta_file)
+
+        self.current_node_id = 0
+        self.nodes = {}
+        self.node_pointers = {}
+
+        roots, broad_nodes, potential_final_root = self.process_cluster(cluster_obj, sequence_map)
+
+        if env.verbosity >= 3:
+            t1 = time.time()
+            cluster_obj.print_all()
+            print(f'Initial process_cluster: {t1-t0}\n{roots}\n{broad_nodes}\n')
+
+        while len(roots) > 1 or len(broad_nodes) > 0:
+            if env.verbosity >= 3:
+                tl0 = time.time()
+            fasta_file = make_fasta(sequence_map, env, subset = roots)
+            cluster_obj = get_mmseqs_cluster(env, fasta_file, seq_id_threshold = 0.0)
+            os.remove(fasta_file)
+
+            new_roots, new_broad_nodes, potential_final_root = self.process_cluster(cluster_obj, sequence_map)
+
+            if env.verbosity >= 4:
+                print(f'New roots coming from: {roots}\n{new_roots}\n')
+
+            for rep in broad_nodes:
+                members, node_id = broad_nodes[rep]
+                fasta_file = make_fasta(sequence_map, env, subset = members)
+                cluster_obj = get_mmseqs_cluster(env, fasta_file, seq_id_threshold = 1.0)
+                os.remove(fasta_file)
+
+                if env.verbosity >= 4:
+                    print(f'Member clustering:\n{members}')
+                    cluster_obj.print_all()
+
+                even_more_new_roots, even_more_new_broad_nodes, _ = self.process_cluster(cluster_obj, sequence_map, add_to_node = node_id)
+
+                if env.verbosity >= 4:
+                    print(f'New roots coming from members:\n{even_more_new_roots}\n')
+
+                new_roots += even_more_new_roots
+                new_broad_nodes.update(even_more_new_broad_nodes)
+
+            if len(new_broad_nodes) == 0 and len(roots) == len(new_roots):
+                for rep in roots:
+                    if not rep in self.nodes:
+                        self.nodes[rep] = self.Node(rep)
+                rep_a = roots[0]
+                if rep_a in self.node_pointers:
+                    left_node_id = self.node_pointers[rep_a]
+                else:
+                    left_node_id = rep_a
+
+                for rep_b in roots[1:]:
+                    children_nodes = [left_node_id]
+
+                    if rep_b in self.node_pointers:
+                        children_nodes.append(self.node_pointers[rep_b])
+                    else:
+                        children_nodes.append(rep_b)
+
+                    new_node_id = self.get_new_node_id()
+                    new_node = self.Node(new_node_id, children = children_nodes)
+                    self.nodes[new_node_id] = new_node
+                    left_node_id = new_node_id
+
+                potential_final_root = new_node_id
+                break
+
+            roots = new_roots
+            broad_nodes = new_broad_nodes
+
+            if env.verbosity >= 3:
+                tl1 = time.time()
+                print(f'Next iteration of process_cluster: {tl1-tl0}\n{roots}\n{broad_nodes}\n')
+
+        self.root = self.nodes[potential_final_root]
+
+    def get_new_node_id(self):
+        self.current_node_id += 1
+        return self.current_node_id
+
+    def process_cluster(self, cluster_obj, sequence_map, add_to_node = None):
+        roots = []
+        broad_nodes = {}
+
+        for rep in cluster_obj.clusters:
+            
+            members = cluster_obj.clusters[rep]
+            if len(members) == 1:
+                if not rep in self.nodes:
+                    self.nodes[rep] = self.Node(rep)
+                if add_to_node is not None:
+                    self.nodes[add_to_node].children.append(rep)
+                else:
+                    roots.append(rep)
+            elif len(members) == 2:
+                if not members[0] in self.nodes:
+                    self.nodes[members[0]] = self.Node(members[0])
+                if not members[1] in self.nodes:
+                    self.nodes[members[1]] = self.Node(members[1])
+
+                children_nodes = []
+
+                if members[0] in self.node_pointers:
+                    children_nodes.append(self.node_pointers[members[0]])
+                else:
+                    children_nodes.append(members[0])
+
+                if members[1] in self.node_pointers:
+                    children_nodes.append(self.node_pointers[members[1]])
+                else:
+                    children_nodes.append(members[1])
+
+                new_node_id = self.get_new_node_id()
+                new_node = self.Node(new_node_id, children = children_nodes)
+                #new_node.fuse_check(sequence_map)
+                self.nodes[new_node_id] = new_node
+                if add_to_node is not None:
+                    self.nodes[add_to_node].children.append(new_node_id)
+                    roots.append(rep)
+                    self.node_pointers[rep] = new_node_id
+
+            elif cluster_obj.seq_id_threshold == 1.0:
+                children_nodes = []
+                for member in members:
+                    if not member in self.nodes:
+                        self.nodes[member] = self.Node(member)
+                    if member in self.node_pointers:
+                        children_nodes.append(self.node_pointers[member])
+                    else:
+                        children_nodes.append(member)
+
+                new_node_id = self.get_new_node_id()
+                new_node = self.Node(new_node_id, fused_children = children_nodes)
+                self.nodes[new_node_id] = new_node
+                if add_to_node is not None:
+                    self.nodes[add_to_node].children.append(new_node_id)
+
+                if add_to_node is None:
+                    roots.append(rep)
+                    self.node_pointers[rep] = new_node_id
+
+            else:
+                
+                new_node_id = self.get_new_node_id()
+                new_node = self.Node(new_node_id)
+                self.nodes[new_node_id] = new_node
+                if add_to_node is not None:
+                    self.nodes[add_to_node].children.append(rep)
+
+                if add_to_node is None:
+                    roots.append(rep)
+                    self.node_pointers[rep] = new_node_id
+                broad_nodes[rep] = members, new_node_id
+        if len(roots) == 1 and add_to_node is None:
+            potential_final_root = new_node_id
+        else:
+            potential_final_root = None
+        return roots, broad_nodes, potential_final_root
+
+    def print_tree(self):
+        self.root.print_cascade(self.nodes)
--- utils.py
+++ utils.py
@@ -1,14 +1,22 @@
 import random
+import os
+import subprocess
+import string
 
 def randomString(stringLength=10):
     """Generate a random string of fixed length """
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(stringLength))
 
-def seqMapToFasta(seq_map, outfile):
+def seqMapToFasta(seq_map, outfile, subset = None):
     lines = []
 
-    for prot_id in seq_map:
+    if subset is None:
+        iterator = seq_map
+    else:
+        iterator = subset
+
+    for prot_id in iterator:
 
         seq = seq_map[prot_id]
         lines.append(f'>{prot_id}\n')
@@ -21,3 +29,62 @@ def seqMapToFasta(seq_map, outfile):
         return True
     else:
         return 'Empty fasta file'
+
+def parseFasta(path=None, new_file=None, lines=None, page=None, left_split=None, right_split=' '):
+    if lines is None and page is None:
+        f = open(path, 'r')
+        lines = f.read().split('\n')
+        f.close()
+    elif lines is None:
+        lines = page.split('\n')
+
+    seq_map = {}
+    n = 0
+
+    if new_file is not None:
+        new_lines = []
+
+    for line in lines:
+        if len(line) == 0:
+            continue
+        if line[0] == '>':
+            entry_id = line[1:]
+            if left_split is not None:
+                entry_id = entry_id.split(left_split, 1)[1]
+            if right_split is not None:
+                entry_id = entry_id.split(right_split, 1)[0]
+            seq_map[entry_id] = ''
+            n += 1
+            if new_file is not None:
+                new_lines.append(line)
+        else:
+            seq_map[entry_id] += line
+            if new_file is not None:
+                new_lines.append(line)
+
+    if new_file is not None:
+        f = open(new_file, 'w')
+        f.write('\n'.join(new_lines))
+        f.close()
+
+    return seq_map
+
+def call_mmseqs_clustering(env, fasta_file, output_path = None, seq_id_threshold = 0.0, silenced = True):
+
+    if output_path is None:
+        infile_trunk = fasta_file.split('/')[-1].rsplit('.',1)[0]
+        output_path = f'{env.out_dir}/{infile_trunk}'
+
+    cmds = [env.mmseqs2_path, 'easy-linclust', fasta_file, output_path, env.tmp_folder, '--similarity-type', '2', '--cov-mode', '0', '-c', '1.0', '--min-seq-id', str(seq_id_threshold)]
+
+    if silenced:
+        FNULL = open(os.devnull, 'w')
+        p = subprocess.Popen(cmds, stdout=FNULL)
+    else:
+        p = subprocess.Popen(cmds)
+    p.wait()
+
+    cluster_file = f'{output_path}_cluster.tsv'
+    rep_seq_file = f'{output_path}_rep_seq.fasta'
+    all_seq_file = f'{output_path}_all_seqs.fasta'
+    return cluster_file, rep_seq_file, all_seq_file
