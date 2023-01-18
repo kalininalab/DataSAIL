b0e8efd3c91ee90a7d4cec5d4017c90efabc74e0
Roman Joeres <joeres.roman@web.de>
2023-01-18 16:17:34 +0100

    Results for Amay, PoC for clustering in preprocessing, code structure for (partially) distance based splitting of clusters

--- amay.py
+++ amay.py
@@ -1,23 +1,43 @@
 import numpy as np
+from sklearn.cluster import AgglomerativeClustering
 
-from scala.bqp.algos.cluster_cold_single import solve_ccx_iqp
+from scala.bqp.algos.cluster_sim_cold_single import solve_ccx_iqp
 
+file = "tests/data/amay/pairwise_distance.tsv"
+# file = "tests/data/pipeline/prot_sim.tsv"
 items = []
-sims = []
-with open("tests/data/amay/pairwise_distance.tsv", "r") as data:
+dists = []
+with open(file, "r") as data:
     for i, line in enumerate(data.readlines()[1:]):
         parts = line.strip().split("\t")
         items.append(parts[0])
-        sims.append([float(x) for x in parts[1:]])
+        dists.append([float(x) for x in parts[1:]])
 
-sims = np.array(sims)
+dists = np.array(dists)
+ca = AgglomerativeClustering(n_clusters=None, metric='precomputed', linkage="average",
+                             distance_threshold=np.average(dists) * 0.9)
+labels = ca.fit_predict(dists)
+
+cluster_dists, cluster_count = np.zeros((max(labels) + 1, max(labels) + 1)), np.zeros(
+    (max(labels) + 1, max(labels) + 1))
+for i in range(len(items)):
+    for j in range(i + 1, len(items)):
+        if labels[i] != labels[j]:
+            cluster_dists[labels[i], labels[j]] += dists[i, j]
+            cluster_count[labels[i], labels[j]] += 1
+
+            cluster_dists[labels[j], labels[i]] += dists[i, j]
+            cluster_count[labels[j], labels[i]] += 1
+
+cluster_dists /= np.max((cluster_count + np.eye(max(labels) + 1), np.ones_like(cluster_count)))
+cluster_sims = 1 - cluster_dists / np.max(cluster_dists)
 
 split = solve_ccx_iqp(
-    clusters=items,
-    weights=[1 for _ in items],
-    similarities=sims,
-    threshold=0.0,
-    limit=0.05,
+    clusters=list(range(max(labels) + 1)),
+    weights=[1 for _ in range(max(labels) + 1)],
+    similarities=cluster_sims,
+    threshold=1.0,
+    limit=0.1,
     splits=[0.8, 0.2],
     names=["train", "test"],
     max_sec=1000,
@@ -25,3 +45,8 @@ split = solve_ccx_iqp(
 )
 
 print(split)
+
+with open("amay.tsv", "w") as out:
+    print("Query", "Split", sep="\t", file=out)
+    for n, i in zip(items, labels):
+        print(n, split[i], sep="\t", file=out)
---
+++ scala/bqp/algos/cluster_dist_cold_single.py
@@ -0,0 +1,81 @@
+import logging
+from typing import List, Optional, Dict
+
+import cvxpy
+import numpy as np
+
+
+def solve_ccx_iqp(
+        clusters: List[str],
+        weights: List[float],
+        distances: np.ndarray,
+        threshold: float,
+        limit: float,
+        splits: List[float],
+        names: List[str],
+        max_sec: int,
+        max_sol: int,
+) -> Optional[Dict[str, str]]:
+    # return None
+
+    print("Defining the optimization problem")
+    alpha = 0.5
+
+    x = {}
+    for i in range(len(clusters)):
+        for b in range(len(splits)):
+            x[i, b] = cvxpy.Variable(boolean=True)
+
+    constraints = []
+
+    for i in range(len(clusters)):
+        constraints.append(sum(x[i, b] for b in range(len(splits))) == 1)
+
+    for b in range(len(splits)):
+        var = sum(x[i, b] * weights[i] for i in range(len(clusters)))
+        constraints.append(int(splits[b] * sum(weights) * (1 - limit)) <= var)
+        constraints.append(var <= int(splits[b] * sum(weights) * (1 + limit)))
+
+    for b in range(len(splits)):
+        for i in range(len(clusters)):
+            for j in range(len(clusters)):
+                constraints.append((x[i, b] - x[j, b]) ** 2 * similarities[i][j] <= threshold)
+
+    cmb = sum(
+        # minimize distance to target size of clusters
+        alpha * (sum(x[i, b] * weights[i] for i in range(len(clusters))) - splits[b] * sum(weights)) ** 2 +
+        # minimize similarities between elements of clusters
+        sum((x[i, b] - x[j, b]) ** 2 * similarities[i][j] for i in range(len(clusters)) for j in range(len(clusters)))
+        for b in range(len(splits))
+    )
+
+    # solve
+    logging.info("Start solving with MOSEK")
+    objective = cvxpy.Minimize(cmb)
+    problem = cvxpy.Problem(objective, constraints)
+    problem.solve(
+        solver=cvxpy.MOSEK,
+        qcp=True,
+        # mosek_params={
+        #     mosek.dparam.optimizer_max_time: max_sec * 1_000,
+        # },
+        # verbose=True,
+    )
+
+    print(f"MOSEK status: {problem.status}")
+    print(f"Solution's score: {problem.value}")
+
+    if problem.status != "optimal":
+        logging.warning(
+            'MOSEK cannot solve the problem. Please consider relaxing split restrictions, '
+            'e.g., less splits, or a higher tolerance level for exceeding cluster limits.'
+        )
+        return None
+
+    output = {}
+    for i in range(len(clusters)):
+        for b in range(len(splits)):
+            if x[i, b].value > 0.1:
+                output[clusters[i]] = names[b]
+
+    return output
