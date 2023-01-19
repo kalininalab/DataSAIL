7853ae9d069ad52416a0f32ca9b42547cc54914a
Roman Joeres <joeres.roman@web.de>
2023-01-19 16:43:21 +0100

    Fix issue with test execution

--- conftest.py
+++ conftest.py
@@ -4,12 +4,10 @@ from tests.data.generate import generate
 
 
 def pytest_sessionstart(session):
-    return
     generate(0.7, 10, "data/perf_7_3")
     generate(0.7, 100, "data/perf_70_30")
 
 
 def pytest_sessionfinish(session):
-    return
     shutil.rmtree("data/perf_7_3")
     shutil.rmtree("data/perf_70_30")
