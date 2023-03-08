72edda63cb0e4547a442d8ac463d41802fd51cce
Roman Joeres <joeres.roman@web.de>
2023-03-08 11:52:23 +0100

    Tests about clustering algorithms

---
+++ datasail/cluster/caching.py
@@ -0,0 +1,2 @@
+def cache():
+    pass
--- tests/test_clustering.py
+++ tests/test_clustering.py
@@ -1,7 +1,14 @@
 import numpy as np
+import pytest
 
+from datasail.cluster.cdhit import run_cdhit
 from datasail.cluster.clustering import additional_clustering
-from datasail.reader.utils import DataSet
+from datasail.cluster.ecfp import run_ecfp
+from datasail.cluster.foldseek import run_foldseek
+from datasail.cluster.mash import run_mash
+from datasail.cluster.mmseqs2 import run_mmseqs
+from datasail.reader.read_proteins import parse_fasta, read_pdb_folder
+from datasail.reader.utils import DataSet, read_csv
 
 
 def test_additional_clustering():
@@ -71,3 +78,70 @@ def test_additional_clustering():
     assert np.min(d_dataset.cluster_distance) == 0
     assert np.max(d_dataset.cluster_distance) == 4
     assert [d_dataset.cluster_weights[i] for i in d_dataset.cluster_names] == [16, 36]
+
+
+@pytest.fixture
+def protein_fasta_data():
+    data = parse_fasta("data/pipeline/seqs.fasta")
+    return DataSet(type="M", data=data, names=list(sorted(data.keys())))
+
+
+@pytest.fixture
+def protein_pdb_data():
+    data = dict((k, v) for k, v in read_pdb_folder("data/pipeline/pdbs"))
+    return DataSet(type="M", data=data, names=list(sorted(data.keys())))
+
+
+@pytest.fixture
+def molecule_data():
+    data = dict((k, v) for k, v in read_csv("data/pipeline/drugs.tsv"))
+    return DataSet(type="M", data=data, names=list(sorted(data.keys())))
+
+
+@pytest.fixture
+def genome_fasta_data():
+    data = parse_fasta("data/pipeline/seqs.fasta")
+    return DataSet(type="M", data=data, names=list(sorted(data.keys())))
+
+
+def test_cdhit_protein(protein_fasta_data):
+    check_clustering(*run_cdhit(protein_fasta_data), protein_fasta_data)
+
+
+@pytest.mark.todo
+def test_cdhit_genome(genome_fasta_data):
+    check_clustering(*run_cdhit(genome_fasta_data), genome_fasta_data)
+
+
+def test_ecfp_molecule(molecule_data):
+    check_clustering(*run_ecfp(molecule_data), molecule_data)
+
+
+def test_foldseek_protein(protein_pdb_data):
+    check_clustering(*run_foldseek(protein_pdb_data), protein_pdb_data)
+
+
+@pytest.mark.todo
+def test_mash_genomic(genome_fasta_data):
+    check_clustering(*run_mash(genome_fasta_data), genome_fasta_data)
+
+
+def test_mmseqs2_protein(protein_fasta_data):
+    check_clustering(*run_mmseqs(protein_fasta_data), protein_fasta_data)
+
+
+def test_wlkernel_protein(protein_pdb_data):
+    check_clustering(*run_mmseqs(protein_pdb_data), protein_pdb_data)
+
+
+def test_wlkernel_molecule(molecule_data):
+    check_clustering(*run_mmseqs(molecule_data), molecule_data)
+
+
+def check_clustering(names, mapping, matrix, dataset):
+    assert list(sorted(names)) == list(sorted(set(mapping.values())))
+    assert list(sorted(mapping.keys())) == list(sorted(dataset.names))
+    assert tuple(matrix.shape) == (len(names), len(names))
+    assert np.min(matrix) >= 0
+    assert np.max(matrix) <= 1
+    assert len(names) <= len(dataset.names)
