8cdffe34ce881673c1c277720e26f6f395806c6b
Roman Joeres <Roman.Joeres@helmholtz-hzi.de>
2023-05-24 13:12:07 +0200

    Bugs fixed introduced by previous commit

--- datasail/reader/read_genomes.py
+++ datasail/reader/read_genomes.py
@@ -38,9 +38,9 @@ def read_genome_data(
             dataset.location = data
         case dict():
             dataset.data = data
-        case Callable():
+        case x if isinstance(x, Callable):
             dataset.data = data()
-        case Generator():
+        case x if isinstance(x, Generator):
             dataset.data = dict(data)
         case _:
             raise ValueError()
