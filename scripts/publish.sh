761f0dfe5d76e8032d97e6214c862ec82e73829b
Roman Joeres <joeres.roman@web.de>
2023-11-06 13:27:32 +0100

    Updated script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,17 +2,19 @@
 
 echo "Prepare conda"
 conda install -y anaconda-client conda-build
+conda list
+anaconda upload -h
 
 echo "Building conda package for Linux"
-conda build --variants "{'python': ['3.8', '3.9', '3.10', '3.11']}" -c conda-forge -c bioconda -c mosek --output-folder . --no-test ../recipe
+conda build --variants "{'python': ['3.8']}" -c conda-forge -c bioconda -c mosek --output-folder . --no-test ../recipe
 
 echo "Convert conda package to other platforms"
 conda convert --platform osx-64 linux-64/*.tar.bz2
-conda convert --platform win-64 linux-64/*.tar.bz2
-conda convert --platform osx-arm64 linux-64/*.tar.bz2
+# conda convert --platform win-64 linux-64/*.tar.bz2
+# conda convert --platform osx-arm64 linux-64/*.tar.bz2
 
 echo "Upload conda package to Anaconda Cloud"
-anaconda upload --label main linux-64/*.tar.bz2
+# anaconda upload --label main linux-64/*.tar.bz2
 anaconda upload --label main osx-64/*.tar.bz2
-anaconda upload --label main win-64/*.tar.bz2
-anaconda upload --label main osx-arm64/*.tar.bz2
+# anaconda upload --label main win-64/*.tar.bz2
+# anaconda upload --label main osx-arm64/*.tar.bz2
